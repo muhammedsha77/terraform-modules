381021ee7e07bbd00ede18216d1889e0bfd55cc7
muhammedshan <decapriode@gmail.com>
2026-01-16 23:33:19 +0530

    Intial_terraformec2module

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+    description = "The region of the instance"
+    type = string
+}
+
+variable "instance_type" {
+    description = "The type of the instance"
+    type = string
+}
+
+variable "instance_name" {
+    description = "The name of the instance"
+    type = string
+}
